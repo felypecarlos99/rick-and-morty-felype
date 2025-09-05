4874e09e2be1dd1aa25581dbb43d9fcb95e0c4bc
felype carlos <felype.carlosilva@gmail.com>
2025-09-05 11:02:52 -0300

    salvar 01 05

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <div className="app">
+      <h1>Personagem Rick and Morty</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <img
+          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+          alt="Rick Sanchez"
+        />
+        <h2>Rick Sanchez</h2>
+        <p><strong>Status:</strong> Alive</p>
+        <p><strong>Espécie:</strong> Human</p>
+        <p><strong>Gênero:</strong> Male</p>
+        <p><strong>Último episódio:</strong> Rickmurai Jack</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
